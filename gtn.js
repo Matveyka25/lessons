a4b350b7bdd1f103e1cca265d4c3cce81905cf35
Matveyka25 <matveygrechin2007@gmail.com>
2024-04-09 19:32:11 +0200

    начало верстки

--- gtn.js
+++ gtn.js
@@ -42,6 +42,71 @@ else{
 
 
 
-let n2 = '260';
+`let n2 = '260';
 let wini = n2[0]
+console.log(wini);`
+
+
+
+function isNegative(n){
+    if (n>0) {
+        console.log("true");
+    }
+    else{
+         console.log("false");
+    }
+}
+isNegative(7);
+
+function showLast(dog){
+console.log(dog.length);
+}
+showLast("dog");
+
+function showLastChar(cat){
+let last = cat[cat.length-1];
+console.log(last);
+}
+showLastChar("last");
+showLastChar('cat');
+
+function showFast(n1){
+if(n1%2==0){
+    console.log(n1);
+}
+}
+showFast(6);
+
+function showWrite(juz){
+let flexFirstChar = flex[0];
+let juzFirstChar = juz[0];
+if (flexFirstChar==juzFirstChar){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+}
+showWrite(3);
+
+function showWatch(good){
+let audi = good[good.length-1];
+if(audi=='ь'){
+    console.log(good[good.length-2]);
+}
+else{
+   console.log("Вы мне должны пол милионна");
+}
+}
+showWatch(2);
+
+function showRust(n3){
+let wini = n3[0]
 console.log(wini);
+}
+showRust("2");
+
+
+
+let n4 = 289;
+if ()
